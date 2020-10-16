fafe1b5eb3f70c38bd8e17bf17128254c92477d7
MasterPK <petrkrehlik@outlook.com>
2020-10-16 17:05:30 +0200

    forms

--- app/WebModule/Presenters/IndexPresenter.php
+++ app/WebModule/Presenters/IndexPresenter.php
@@ -7,7 +7,10 @@ namespace App\WebModule\Presenters;
 use App\Storm\Product;
 use App\Storm\ProductRepository;
 use App\Storm\UserRepository;
+use Forms\Forms;
+use Nette\Application\UI\Form;
 use Nette\Application\UI\Presenter;
+use Nette\Utils\DateTime;
 use Pages\DB\IPageRepository;
 use StORM\DIConnection;
 use Pages\Pages;
@@ -35,6 +38,38 @@ final class IndexPresenter extends Presenter
 	
 	/** @var Pages @inject */
 	public Pages $pages;
+	
+	/** @var \Forms\Forms @inject */
+	public Forms $forms;
+	
+	public function createComponentForm($name): Form
+	{
+		$form = $this->forms->createForm();
+		$form->addMutationSelector('Zvolte jazyk');
+		$form->addLocaleText("name","Name");
+		$form->addDate("date","Date", ["defaultDate"=> (new DateTime())]);
+		$form->addDateRange("range","Range");
+		$form->addRange("posuvnik","Posuvnik:");
+		$form->addDataMultiSelect("dataselect","Vyber:",["test1"=>"test1","test2"=>"test2","test3"=>"test3"])->setHtmlAttribute("style","margin: 60px;");
+		$form->addRichEdit('content', 'Obsah:');
+		$form->addColor("color");
+		$imagePicker = $form->addImagePicker('imagePicker', 'Image Picker:', [
+			'adresar' => null,
+			'adresarResized' => static function (Image $image): void {
+				$image->resize(24, 24, Image::EXACT);
+			}], 'Náhravejte čtvercové obrázky');
+		$form->addAntispam('Špatně vyplněný antispam');
+		$form->addDoubleClickProtection('Formular byl odeslan vicekrat');
+		
+		//$form->addTranslatedCheckbox('Aktivni');
+		$form->addSubmit("submit");
+		$form->onSuccess[]=function (Form $form){
+			Debugger::barDump($form->getValues());
+			$form->reset();
+			$this->redirect("this");
+		};
+		return $form;
+	}
 	
 	public function actionDefault()
 	{
@@ -79,4 +114,5 @@ final class IndexPresenter extends Presenter
 		//$this->productRepo->createOne(["name"=>["cz"=>"dvere","en"=>"doors"]]);
 		
 	}
+	
 }
